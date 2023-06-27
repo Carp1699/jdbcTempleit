68df352c1ba546ca34bc01fd44ddd10e34420f20
Carp1699 <128076782+Carp1699@users.noreply.github.com>
2023-06-27 15:25:43 -0600

    Add files via upload

---
+++ jdbc/src/main/java/com/amigoscode/movie/MovieDao.java
@@ -0,0 +1,13 @@
+package com.amigoscode.movie;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MovieDao {
+    List<Movie> selectMovies();
+    int insertMovie(Movie movie);
+    int deleteMovie(int id);
+    Optional<Movie> selectMovieById(int id);
+    // TODO: Update
+    void updateMovie(Movie movie,int id);
+}
